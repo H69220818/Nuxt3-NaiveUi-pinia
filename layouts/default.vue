b78329e33acd0fdf7dfe917f44c12e2edc14cfe2
huzaijing <44773769+H69220818@users.noreply.github.com>
2022-08-01 10:33:10 +0800

    优化header布局，修改滚动条
    优化header布局，修改滚动条

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,31 @@
 
 <template>
     <div class="default">
-        <TheHeader />
-        <slot />
-        <TheFooter />
+        <div class="header">
+            <TheHeader />
+        </div>
+        <div class="content">
+            <slot />
+            <TheFooter />
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.default {
+    width: 100%;
+    box-sizing: border-box;
+
+    .header {
+        background: #fff;
+        width: 100%;
+        position: fixed;
+        top: 0;
+    }
+
+    .content {
+        // margin-top: 65px;
+        padding-top: 65px;
+    }
+}
 </style>
